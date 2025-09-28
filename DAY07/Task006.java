3c54c50944dbea695778b969f901b3ddcf55383d
Avinash H <115771542+avinashhosagoudar@users.noreply.github.com>
2025-09-28 14:11:25 +0530

    Java Codes of compiler, variable, datatypes

--- DAY07/Task006.java
+++ DAY07/Task006.java
@@ -2,8 +2,10 @@ package DAY07;
 
 public class Task006 {
     public static void main(String[]args){
+        
         int a =20;
         int b= 30;
         System.out.println((a > b) ? "A is greater" : "b is greater");
     }
 }
+
