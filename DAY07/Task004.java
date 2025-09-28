dfdf9dfab49dcb068988f4b4b47b19392a7f7060
Avinash H <115771542+avinashhosagoudar@users.noreply.github.com>
2025-09-28 14:10:50 +0530

    Java Codes of compiler, variable, datatypes

--- DAY07/Task004.java
+++ DAY07/Task004.java
@@ -2,6 +2,7 @@ package DAY07;
 
 public class Task004 {
     public static void main(String[]args){
+        
         int a=2;
         int b=5;
         System.out.println("before:" + "a="+ a + "b=" +b );
@@ -13,3 +14,4 @@ public class Task004 {
 
     }
 }
+
