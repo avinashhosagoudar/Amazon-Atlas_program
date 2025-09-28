6feaec1caf3528b545833854e0bb03a8311a858b
Avinash H <115771542+avinashhosagoudar@users.noreply.github.com>
2025-09-28 15:57:35 +0530

    Delete DAY37 directory

--- DAY37/Task001/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package DAY37.Task001;
-
-
-public interface UserRepository {
-    User02 findById(Long id);
-    User02 save(User02 user);
-}
--- DAY37/Task003/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package DAY37.Task003;
-
-public class BookRepository {
-    public int getBookCount() {
-        System.out.println("assume calling real getBookCount() from the database.");
-        return 10;
-    }
-}
