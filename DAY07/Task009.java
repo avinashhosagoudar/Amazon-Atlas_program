ff28b11fa0a2b850ab5678f07929d41feb3f719f
Avinash H <115771542+avinashhosagoudar@users.noreply.github.com>
2025-09-28 14:12:04 +0530

    Java Codes of compiler, variable, datatypes

--- DAY07/Task009.java
+++ DAY07/Task009.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Task009 {
 
         public static void main(String[] args) {
+                
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter value of a = ");
             int a = sc.nextInt();
@@ -21,5 +22,6 @@ public class Task009 {
 
         }
     }
+
 
 
