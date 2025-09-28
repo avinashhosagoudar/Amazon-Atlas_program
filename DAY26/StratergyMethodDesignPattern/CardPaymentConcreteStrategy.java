51da0142f6cd77e361480c55443c8e63e4924a11
Avinash H <115771542+avinashhosagoudar@users.noreply.github.com>
2025-09-28 15:51:12 +0530

    Delete DAY26 directory

--- DAY26/StratergyMethodDesignPattern/CardPaymentConcreteStrategy.java
+++
@@ -1,7 +0,0 @@
-package DAY26.StratergyMethodDesignPattern;
-
-public class CardPaymentConcreteStrategy implements PaymentStrategy {
-    public void process(double price) {
-        System.out.println("payment processing using card");
-    }
-}
--- DAY26/StratergyMethodDesignPattern/PaymentStrategy.java
+++
@@ -1,6 +0,0 @@
-package DAY26.StratergyMethodDesignPattern;
-
-public interface PaymentStrategy {
-    void process(double price);
-}
-
